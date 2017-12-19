900ca3ef345a44cf13823d9dcb6f77e914bfedc7
Laskin00 <ale.laskintues19@gmail.com>
2017-12-19 20:39:48 +0200

    something working

--- app/controllers/encrypt_controller.rb
+++ app/controllers/encrypt_controller.rb
@@ -1,2 +1,14 @@
+require 'openssl'
+
 class EncryptController < ApplicationController
+  def encrypt
+    message = params[:message]
+    key = Rsa.find(params[:id]).public_key
+    encrypted_message = key.public_encrypt(message);
+    new_message = Encrypted_message.create(:encrypted_message => encrypted_message)
+    render plain: new_message.id
+  end
+
+  def show
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   post 'rsas' => 'rsa#new_key'
   get 'rsas/:id' => 'rsa#show'
-  post 'rsas/:id/encrypt_messages' => 'encrypt#encrypt'
+  post 'rsas/:id/encrypt_messages/:message' => 'encrypt#encrypt'
   get 'rsas/:id/encrypt_messages/:id' => 'encrypt#show'
-  post 'rsas/:id/decrypt_messages' => 'decrypt#decrypt'
+  post 'rsas/:id/decrypt_messages/:message' => 'decrypt#decrypt'
 end
