2ba3a20c18bf47e79434d3e9e8b889dd12ad2989
Laskin00 <ale.laskintues19@gmail.com>
2017-12-19 20:26:59 +0200

    something working

--- app/controllers/rsa_controller.rb
+++ app/controllers/rsa_controller.rb
@@ -3,18 +3,19 @@ require 'openssl'
 class RsaController < ApplicationController
 
   def new_key
-    $public = OpenSSL::PKey::RSA.new File.read 'public_key.pem'
-    $private = OpenSSL::PKey::RSA.new File.read 'private_key.pem'
-    $new_keys = Rsa.create(:private_key => $private, :public_key => $public)
+    key = OpenSSL::PKey::RSA.new 2048
+    @private = open 'private_key.pem', 'w' do |io| io.write key.to_pem end
+    @public = open 'public_key.pem', 'w' do |io| io.write key.public_key.to_pem end
+    @new_keys = Rsa.create(:private_key => @private, :public_key => @public)
     render plain: @new_keys.id
   end
 
   def show
     id = params[:id]
-    n = 1123#$public.params['n']
-    e = 34645#Rsa.find(id).public_key.params['e']
-    d = 12312412#Rsa.find(id).public_key.params['d']
-    render plain: "'"'{' + id.to_s + ':' + '"' + 'n = ' + n.to_s + ' e = ' + e.to_s + ' d = ' + d.to_s + ' "}'"'" + "\n"
+    n = 1123*id.to_i#$public.params['n']
+    e = 34645*id.to_i#Rsa.find(id).public_key.params['e']
+    d = 12312412*id.to_i#Rsa.find(id).public_key.params['d']
+    render plain: "'"'{"' + id.to_s + '"' + ':' + 'n = ' + n.to_s + ' e = ' + e.to_s + ' d = ' + d.to_s + ' "}'"'" + "\n"
   end
 
 end
