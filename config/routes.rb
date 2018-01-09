86782e1b18d8d04e689472a41d449e26390ec1b6
Laskin00 <ale.laskintues19@gmail.com>
2018-01-09 15:14:59 +0200

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   post 'rsas' => 'rsa#key_with_parrams_given'
   post 'rsas' => 'rsa#new_key'
-  post 'rsas/:id/encrypt_message' => 'encrypt#encrypt'
+  post 'rsas/:id/encrypt_messages' => 'encrypt#encrypt'
   post 'rsas/:id/decrypt_messages' => 'decrypt#decrypt'
   get 'rsas/:id' => 'rsa#show'
   get 'rsas/:id/encrypt_messages/:id2' => 'encrypt#show'
